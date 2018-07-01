1dcbbe8b919fe6a9b0e1e38ce51095c48c041adf
Simon Nguyen <simonnguyen3054@gmail.com>
2018-06-30 18:35:31 -0700

    Add display result function when time is out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,12 @@ var data = [
   {
     question: 'How many players are in a soccer team?',
     answers: [11, 9, 10, 12],
-    correctAnswer: 11
+    correctAnswer: '11'
   },
   {
     question: 'How many teams are in the 2018 World Cup Tourament',
     answers: [36, 30, 32, 40],
-    correctAnswer: 36
+    correctAnswer: '36'
   },
   {
     question: 'How many teams are in the 2018 World Cup Tourament',
@@ -19,22 +19,32 @@ var data = [
 
 var questions;
 var userChoice = []; //array containing user's choices
-var correctAnswer = 0;
 var timerTracker = 0;
 
-//Create 30 sec timer
+//create a correct answers array
+function correctAnswersArr(dataObj) {
+  var correctAnswers = []; //array container correct answers
+
+  for (var i in dataObj) {
+    correctAnswers.push(dataObj[i].correctAnswer);
+  }
+
+  return correctAnswers;
+}
+
 function timer() {
-  var sec = 30;
+  var sec = 5;
   var timer = setInterval(function() {
     document.getElementById('timer').innerHTML = sec;
     sec--;
     if (sec < 0) {
       clearInterval(timer);
+      displayResult(); //when time hit 0, run this function
     }
   }, 1000);
 }
 
-function createAnswersList(answersArray) {
+function createAnswersList(answersArray, questionIndex) {
   //display answers
   var answerList = $('<ul>');
   var item;
@@ -43,7 +53,8 @@ function createAnswersList(answersArray) {
   for (var answerItem in answersArray) {
     var choice = answersArray[answerItem];
     item = $('<li>');
-    input = '<input type="radio" name="answer">';
+    input = `<input type="radio" name="question-${parseInt(questionIndex) +
+      1}" value="${choice}">`;
     input += choice;
     item.append(input);
     answerList.append(item);
@@ -61,42 +72,67 @@ function createQuestionEle(quiz) {
     questionAnswersEle.append(questionsEle);
 
     //display answers
-    var answerList = createAnswersList(quiz[index].answers);
+    var answerList = createAnswersList(quiz[index].answers, index);
     questionAnswersEle.append(answerList);
   }
 
   return questionAnswersEle;
 }
 
-//When Start button is click
-//display all questions
+//display result when time ran out
+function displayResult() {
+  var correctCounter = 0;
+  var wrongCounter = 0;
+  var unanswerCounter = 0;
+
+  var questionNumber = 1; //Start with question number 1
+  while (questionNumber <= data.length) { //Run this up to questionNumber 10
+    var correctChoice = data[questionNumber - 1].correctAnswer; //Get correct answer
+    //find all radios within a question (should have 4 radios)
+    var radioInputs = $(`input[name="question-${questionNumber}"]`);
+    var radioNotchecked = 0;
+
+    //loop through radios too see if checked
+    for (var i = 0; i < radioInputs.length; i++) {
+      if (radioInputs[i].checked) {
+        //check to see if the answered is correct
+        var userChoice = radioInputs[i].value;
+        if (userChoice === correctChoice) {
+          correctCounter++;
+        } else {
+          wrongCounter++;
+        }
+      } else {
+        radioNotchecked++;
+        if (radioNotchecked === 4) {
+          unanswerCounter++;
+        }
+      }
+    }
+    questionNumber++;
+  }
+
+  console.log(correctCounter);
+  console.log(wrongCounter);
+  console.log(unanswerCounter);
+}
+
+//When Start button is clicked, display all questions
 $('#start').on('click', function() {
+  //start timer. When timer hit 0, displayResult function will run
+  timer();
+
   questions = $('#questions');
-
   questions.removeClass(); //remove the class 'hide' to show the questions
-
   $(this).remove(); //remove the start button
 
   var DoneButton = $('<button id="done">Done</button>'); //Add Done button
-  $('#button').append(DoneButton);
+  $('#button').append(DoneButton); //Display the Done button
 
   //display the questions
   var displayQuestions = createQuestionEle(data);
   questions.append(displayQuestions);
 
+  $('#done').on('click', displayResult);
 });
-//each question should have answers as radio buttons
-//start timer
 
-//When Submit button is clicked, read all the selected answers
-//push selected answers into userChoice array
-//loop through userChoice array
-//compare the correctAnswer of each index of the data object with each index of the user choice array
-//if both indices are the same, add to correct answer variable
-
-//If timer hits 0, stop the quiz
-//read all the selected answers
-//push selected answers into userChoice array
-//loop through userChoice array
-//compare the correctAnswer of each index of the data object with each index of the user choice array
-//if both indices are the same, add to correct answer variable
